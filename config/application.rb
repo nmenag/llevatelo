063fa6427a4883a31a8a0171ac0dcd42ae38ad0b
Nicolas Mena <nmena.garzon@gmail.com>
2017-01-08 22:54:39 -0500

    disabled generate assets and helper

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ Bundler.require(*Rails.groups)
 
 module Llevatelo
   class Application < Rails::Application
+    config.generators.assets = false
+    config.generators.helper = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
