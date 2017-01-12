1ac4069af7fe8d3b302a6aed8222798b33c7ad4f
Nicolas Mena <nmena.garzon@gmail.com>
2017-01-11 22:06:01 -0500

    Add select to search form

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,15 @@
 module PagesHelper
+  def generate_list(enum)
+    list = {}
+
+    enum.keys.each do |type|
+      list[type] = type
+    end
+
+    list.map { |k, v| [v, k] }
+  end
+
+  def text_location(value)
+    I18n.t(value)
+  end
 end
