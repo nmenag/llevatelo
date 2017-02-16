f24ce38e81b4e04d4b6a84764ed1bf92374efe7c
Nicolas Mena <nmena.garzon@gmail.com>
2017-02-15 23:06:04 -0500

    Add tests for login of user

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "SignIn", type: :feature do
+
   scenario 'sign in page ' do
     visit new_user_session_path
 
@@ -28,4 +29,66 @@ RSpec.feature "SignIn", type: :feature do
     all('.panel-body a').last.click
     expect(current_path).to eq new_user_password_path
   end
+
+  context 'login' do
+    let!(:user){ create(:user) }
+
+    scenario 'with credentials invalid' do
+      password = Faker::Internet.password
+      sign_in_form(
+        email: user.email,
+        password: password
+      )
+
+      within :css, '#flash_alert' do
+        expect(page).to have_content 'Email o contraseña inválidos.'
+      end
+    end
+
+    scenario 'with credentials invalid' do
+      password = Faker::Internet.password
+      sign_in_form(
+        email: user.email,
+        password: password
+      )
+
+      within :css, '#flash_alert' do
+        expect(page).to have_content 'Email o contraseña inválidos.'
+      end
+    end
+
+    scenario 'access user registered' do
+      login(user)
+
+      expect(current_path).to eq root_path
+
+      within :css, '.dropdown' do
+        expect(page).to have_content user.email
+      end
+
+      within :css, '.dropdown-menu' do
+        expect(page).to have_content 'Mis Publicaciones'
+        expect(page).to have_content 'Editar'
+        expect(page).to have_content 'Perfil'
+        expect(page).to have_content 'Salir'
+      end
+    end
+
+    scenario 'sign out' do
+      login(user)
+      click_link 'Salir'
+      expect(page).to have_selector '.sign-in'
+
+      within :css, '.sign-in' do
+        expect(page).to have_content 'Login'
+      end
+    end
+  end
+end
+
+def sign_in_form(opts={})
+  visit new_user_session_path
+  fill_in 'user_email', with: opts[:email]
+  fill_in 'user_password', with: opts[:password]
+  click_button "Ingresar"
 end
