f53a0e62747699fd7ab5e61fd60a749abe87cb74
Nicolas Mena <nmena.garzon@gmail.com>
2017-02-23 23:01:34 -0500

    Add validations form of article

--- app/assets/javascripts/validations.js
+++ app/assets/javascripts/validations.js
@@ -1,5 +1,5 @@
 $(document).on('turbolinks:load', function() {
-  $('#new_user').validate({
+  $('#new_user, .edit_user').validate({
     rules: {
      'user[email]': 'required',
      'user[password]': 'required',
@@ -13,17 +13,12 @@ $(document).on('turbolinks:load', function() {
     },
   });
 
-  $('#edit_user').validate({
+  $('#new_article, .edit_article').validate({
     rules: {
-     'user[email]': 'required',
-     'user[password]': 'required',
-     'user[phone]': 'required',
-     'user[password_confirmation]': 'required'
-    },
-    messages: {
-      'user[email]': 'Ingresa un correo electrónico',
-      'user[password]': 'Ingresa tu contraseña',
-      'user[password_confirmation]': 'Por favor confirme la contraseña',
-    },
+     'article[name]': 'required',
+     'article[offer_type]': 'required',
+     'article[description]': 'required',
+     'article[status]': 'required'
+    }
   });
-})
+});
