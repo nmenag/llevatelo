eda5fef3cbc88ec9a5a2f22eb7b9864892606390
Nicolas Mena <nmena.garzon@gmail.com>
2017-02-25 21:32:35 -0500

    Add flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,23 @@ module ApplicationHelper
   def text_location(value)
     I18n.t(value) if value.present?
   end
+
+  def bootstrap_class_for(flash_type)
+    hash_class_bootrap = { success: 'alert-success', error: 'alert-danger', alert: 'alert-warning', notice: 'alert-info' }
+    hash_class_bootrap[flash_type.to_sym] || flash_type.to_s
+  end
+
+  def flash_messages(opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "col-xs-11 col-sm-4 alert #{bootstrap_class_for(msg_type)} fade in") do
+        concat content_tag(
+          :button, 'x', class: "close", data: { dismiss: 'alert' })
+            concat message
+          end
+        )
+    end
+    nil
+  end
+
+
 end
