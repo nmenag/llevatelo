fdc8c7b30cebd17ca9e262ebdae37128ce146784
Nicolas Mena <nmena.garzon@gmail.com>
2017-05-25 18:00:42 -0500

    Display articles without user signed in

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
 
   def index
   end
--- app/helpers/favorites_helper.rb
+++ app/helpers/favorites_helper.rb
@@ -1,6 +1,6 @@
 module FavoritesHelper
   def favorited_button(article)
-    if current_user.can_add_favorite?(article)
+    if user_signed_in? && current_user.can_add_favorite?(article)
       if current_user.favorite_articles.exists?(id: @article.id)
         link_to '<i class="glyphicon glyphicon-heart size-32"></i>'.html_safe, favorite_path(@article), method: :delete, class: 'pull-right'
       else
