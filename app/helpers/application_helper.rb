1cce665faff3f08c5dcdc90e5ff6c0d4804a63fc
Nicolas Mena <nmena.garzon@gmail.com>
2017-09-14 01:20:22 -0500

    Add text and label type to article list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,8 +31,8 @@ module ApplicationHelper
   end
 
   def badge_offer_type_button(offer_type)
-    hash_class = {gift: 'warning', want: 'danger', barter: 'primary' }
-    content_tag(:span, class: "label label-#{hash_class[offer_type.to_sym]}") do
+    hash_class = {gift: 'given', want: 'request', barter: 'change' }
+    content_tag(:figure, class: "alert btn-#{hash_class[offer_type.to_sym]}") do
       concat text_location(offer_type)
     end
   end
