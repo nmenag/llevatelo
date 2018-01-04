2705cf4bbcddd84754fd6690a73bd887a747ba2c
Nicolas Mena <nmena.garzon@gmail.com>
2018-01-04 18:19:13 -0500

    change some copies of mailers and modals

--- app/mailers/articles_mailer.rb
+++ app/mailers/articles_mailer.rb
@@ -1,7 +1,8 @@
 class ArticlesMailer < ApplicationMailer
   def contact(article, user)
+    @article = article
     @user_interesed = user
-    @user = article.user
+    @user = @article.user
     mail(to: @user.email,  subject: 'Alguien esta interesado por el producto publicado en llevatelo.co')
   end
 end
