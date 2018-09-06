b3ba4560e4bb11925e690b0b8d5168b6a013bbdb
Nicolas Mena <nmena.garzon@gmail.com>
2018-09-06 16:04:32 -0500

    fix up display image (#36)

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,7 +1,7 @@
 module PagesHelper
   def display_image_article(article, version_name = nil)
     if article.image_main.present?
-      article.image_main.image_file_name
+      article.image_main.image_file_name.url
     else
       "/images/" + [version_name, "cover.png"].compact.join('_')
     end
